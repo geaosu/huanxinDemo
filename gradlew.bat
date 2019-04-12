b70fa8adcc8428764613ed5f2880482c6b1e9d5f
geaosu <geaosu@github.com>
2019-04-12 18:43:59 +0800

    更新

---
+++ gradlew.bat
@@ -0,0 +1,54 @@
+<application>
+  <component name="RecentProjectsManager">
+    <option name="recentPaths">
+      <list>
+        <option value="E:/project/studio 3.2.1/Gank" />
+        <option value="E:/project/studio 3.2.1/App" />
+        <option value="E:/project/studio 3.2.1/WanAndroid" />
+      </list>
+    </option>
+    <option name="openPaths">
+      <list>
+        <option value="E:/project/studio 3.2.1/Gank" />
+      </list>
+    </option>
+    <option name="lastPath" value="E:/project/studio 3.2.1/Gank" />
+    <option name="pid" value="" />
+    <option name="additionalInfo">
+      <map>
+        <entry key="E:/project/studio 3.2.1/App">
+          <value>
+            <RecentProjectMetaInfo>
+              <option name="build" value="AI-181.5540.7.32.5056338" />
+              <option name="productionCode" value="AI" />
+              <option name="binFolder" value="$APPLICATION_HOME_DIR$/bin" />
+              <option name="projectOpenTimestamp" value="1550676835633" />
+              <option name="buildTimestamp" value="1539049491423" />
+            </RecentProjectMetaInfo>
+          </value>
+        </entry>
+        <entry key="E:/project/studio 3.2.1/Gank">
+          <value>
+            <RecentProjectMetaInfo>
+              <option name="build" value="AI-181.5540.7.32.5056338" />
+              <option name="productionCode" value="AI" />
+              <option name="binFolder" value="$APPLICATION_HOME_DIR$/bin" />
+              <option name="projectOpenTimestamp" value="1550939132100" />
+              <option name="buildTimestamp" value="1539049496961" />
+            </RecentProjectMetaInfo>
+          </value>
+        </entry>
+        <entry key="E:/project/studio 3.2.1/My Application">
+          <value>
+            <RecentProjectMetaInfo>
+              <option name="build" value="AI-181.5540.7.32.5056338" />
+              <option name="productionCode" value="AI" />
+              <option name="binFolder" value="$APPLICATION_HOME_DIR$/bin" />
+              <option name="projectOpenTimestamp" value="1540188700214" />
+              <option name="buildTimestamp" value="1539049471613" />
+            </RecentProjectMetaInfo>
+          </value>
+        </entry>
+        <entry key="E:/project/studio 3.2.1/WanAndroid">
+          <value>
+            
